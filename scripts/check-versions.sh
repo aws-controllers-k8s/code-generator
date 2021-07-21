c876663b6a2a2e48d146ad232ca276ba5574c911
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2021-07-21 11:28:13 -0700

    Add script and Github workflow for ensuring version in sync (#130)
    Adds a script that compares the `aws-controllers-k8s/runtime` version listed in the `go.mod` is identical to the `VERSION` variable in the `Makefile`. This way we can ensure the generation metadata is kept in line with the version of the runtime it was built against.

---
+++ scripts/check-versions.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# A script that ensures the code-generator binary version matches its associated
+# runtime version
+
+set -eo pipefail
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR="$SCRIPTS_DIR/.."
+
+USAGE="
+Usage:
+  $(basename "$0") <Makefile version>
+
+Checks that the code-generator version located in the Makefile matches the
+runtime version pinned in go.mod
+"
+
+if [ $# -ne 1 ]; then
+    echo "ERROR: $(basename "$0") only accepts a single parameter" 1>&2
+    echo "$USAGE"
+    exit 1
+fi
+
+RUNTIME_DEPENDENCY_VERSION="aws-controllers-k8s/runtime"
+GO_MOD_VERSION=$(go list -m -f '{{ .Version }}' github.com/aws-controllers-k8s/runtime)
+
+if [[ "$1" != "$GO_MOD_VERSION" ]]; then
+    echo "Code-generator version in Makefile $1 does not match $RUNTIME_DEPENDENCY_VERSION version $GO_MOD_VERSION"
+    exit 1
+fi
+
+exit 0
