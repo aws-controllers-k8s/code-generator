2cad61bf12623c66f36a933c9b72ae8643995728
Tenglong Gu <brucegu@amazon.com>
2022-06-09 17:53:36 +0000

    Add helper function so go template can create slice

--- pkg/generate/ack/controller.go
+++ pkg/generate/ack/controller.go
@@ -169,6 +169,9 @@ var (
 		"CheckNilReferencesPath": func(f *ackmodel.Field, sourceVarName string) string {
 			return code.CheckNilReferencesPath(f, sourceVarName)
 		},
+                "MakeSlice": func (args ...interface{}) []interface{} {
+                        return args
+                },
 	}
 )
 
