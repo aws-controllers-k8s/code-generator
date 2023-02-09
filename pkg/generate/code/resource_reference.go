b868a3680c8910c8236e4dd6a5b0950b8de28b4a
Amine Hilaly <hilalyamine@gmail.com>
2023-02-09 23:29:48 +0100

    Revert "Refactor reference resolution into Go code (#401)"
    This reverts commit 8dc09b7cf894b19b02f60174a31707f0ca6c27b0.

--- pkg/generate/code/resource_reference.go
+++ pkg/generate/code/resource_reference.go
@@ -98,7 +98,7 @@ func ReferenceFieldsValidation(
 			out += fmt.Sprintf("%sif %s.%s != nil"+
 				" && %s.%s != nil {\n", fIndent, pathVarPrefix, field.GetReferenceFieldName().Camel, pathVarPrefix, field.Names.Camel)
 			out += fmt.Sprintf("%s\treturn "+
-				"ackerr.ResourceReferenceAndIDNotSupportedFor(%q, %q)\n",
+				"ackerr.ResourceReferenceAndIDNotSupportedFor(\"%s\", \"%s\")\n",
 				fIndent, field.Path, field.ReferenceFieldPath())
 
 			// Close out all the curly braces with proper indentation
@@ -117,7 +117,7 @@ func ReferenceFieldsValidation(
 					" %s.%s == nil {\n", fIndent, pathVarPrefix,
 					field.ReferenceFieldPath(), pathVarPrefix, field.Path)
 				out += fmt.Sprintf("%s\treturn "+
-					"ackerr.ResourceReferenceOrIDRequiredFor(%q, %q)\n",
+					"ackerr.ResourceReferenceOrIDRequiredFor(\"%s\", \"%s\")\n",
 					fIndent, field.Path, field.ReferenceFieldPath())
 				out += fmt.Sprintf("%s}\n", fIndent)
 			}
@@ -183,111 +183,6 @@ func ReferenceFieldsPresent(
 		}
 	}
 	return iteratorsOut + returnOut
-}
-
-// ResolveReferencesForField produces Go code for accessing all references that
-// are related to the given concrete field, determining whether its in a valid
-// condition and updating the concrete field with the referenced value.
-// Sample code (resolving a nested singular reference):
-//
-// ```
-//
-//	if ko.Spec.JWTConfiguration != nil {
-//		if ko.Spec.JWTConfiguration.IssuerRef != nil && ko.Spec.JWTConfiguration.IssuerRef.From != nil {
-//			arr := ko.Spec.JWTConfiguration.IssuerRef.From
-//			if arr == nil || arr.Name == nil || *arr.Name == "" {
-//				return fmt.Errorf("provided resource reference is nil or empty: \"JWTConfiguration.IssuerRef"\")
-//			}
-//			obj := &svcapitypes.API{}
-//			if err := getReferencedResourceState_API(ctx, apiReader, obj, *arr.Name, namespace); err != nil {
-//				return err
-//			}
-//			ko.Spec.JWTConfiguration.Issuer = obj.Status.APIID
-//		}
-//	}
-//
-// ```
-func ResolveReferencesForField(field *model.Field, sourceVarName string, indentLevel int) string {
-	r := field.CRD
-	fp := fieldpath.FromString(field.Path)
-
-	outPrefix := ""
-	outSuffix := ""
-
-	fieldAccessPrefix := fmt.Sprintf("%s%s", sourceVarName, r.Config().PrefixConfig.SpecField)
-	targetVarName := fmt.Sprintf("%s.%s", fieldAccessPrefix, field.Path)
-
-	for idx := 0; idx < fp.Size(); idx++ {
-		curFP := fp.CopyAt(idx).String()
-		cur, ok := r.Fields[curFP]
-		if !ok {
-			panic(fmt.Sprintf("unable to find field with path %q. crd: %q", curFP, r.Kind))
-		}
-
-		ref := cur.ShapeRef
-
-		indent := strings.Repeat("\t", indentLevel+idx)
-
-		switch ref.Shape.Type {
-		case ("structure"):
-			fieldAccessPrefix = fmt.Sprintf("%s.%s", fieldAccessPrefix, fp.At(idx))
-
-			outPrefix += fmt.Sprintf("%sif %s != nil {\n", indent, fieldAccessPrefix)
-			outSuffix = fmt.Sprintf("%s}\n%s", indent, outSuffix)
-		case ("list"):
-			if (fp.Size() - idx) > 1 {
-				// TODO(nithomso): add support for structs nested within lists
-				// The logic for structs nested within lists needs to not only
-				// be added here, but also in a custom patching solution since
-				// it isn't supported by `StrategicMergePatch`
-				// see https://github.com/aws-controllers-k8s/community/issues/1291
-				panic(fmt.Errorf("references within lists inside lists aren't supported. crd: %q, path: %q", r.Kind, field.Path))
-			}
-			fieldAccessPrefix = fmt.Sprintf("%s.%s", fieldAccessPrefix, fp.At(idx))
-
-			iterVarName := fmt.Sprintf("iter%d", idx)
-
-			// base case for references in a list
-			outPrefix += fmt.Sprintf("%s%s = %s{}\n", indent, targetVarName, field.GoType)
-			outPrefix += fmt.Sprintf("%sfor _, %s := range %s {\n", indent, iterVarName, fieldAccessPrefix)
-
-			fieldAccessPrefix = iterVarName
-			outPrefix += fmt.Sprintf("%s\tarr := %s.From\n", indent, fieldAccessPrefix)
-			outPrefix += fmt.Sprintf("%s\tif arr == nil || arr.Name == nil || *arr.Name == \"\" {\n", indent)
-			outPrefix += fmt.Sprintf("%s\t\treturn fmt.Errorf(\"provided resource reference is nil or empty: \\%q\\\")\n", indent, field.ReferenceFieldPath())
-			outPrefix += fmt.Sprintf("%s\t}\n", indent)
-
-			outPrefix += fmt.Sprintf("%s\tif err := getReferencedResourceState_%s(ctx, apiReader, obj, *arr.Name, namespace); err != nil {\n", indent, field.FieldConfig.References.Resource)
-			outPrefix += fmt.Sprintf("%s\t\treturn err\n", indent)
-			outPrefix += fmt.Sprintf("%s\t}\n", indent)
-			outPrefix += fmt.Sprintf("%s\t%s = append(%s, obj.%s)\n", indent, targetVarName, targetVarName, field.FieldConfig.References.Path)
-			outPrefix += fmt.Sprintf("%s}\n", indent)
-		case ("map"):
-			panic("references cannot be within a map")
-		default:
-			// base case for single references
-			fieldAccessPrefix = fmt.Sprintf("%s.%s", fieldAccessPrefix, cur.GetReferenceFieldName().Camel)
-
-			outPrefix += fmt.Sprintf("%sif %s != nil && %s.From != nil {\n", indent, fieldAccessPrefix, fieldAccessPrefix)
-			outPrefix += fmt.Sprintf("%s\tarr := %s.From\n", indent, fieldAccessPrefix)
-			outPrefix += fmt.Sprintf("%s\tif arr == nil || arr.Name == nil || *arr.Name == \"\" {\n", indent)
-			outPrefix += fmt.Sprintf("%s\t\treturn fmt.Errorf(\"provided resource reference is nil or empty: \\%q\\\")\n", indent, field.ReferenceFieldPath())
-			outPrefix += fmt.Sprintf("%s\t}\n", indent)
-
-			if field.FieldConfig.References.ServiceName == "" {
-				outPrefix += fmt.Sprintf("%s\tobj := &svcapitypes.%s{}\n", indent, field.FieldConfig.References.Resource)
-			} else {
-				outPrefix += fmt.Sprintf("%s\tobj := &%sapitypes.%s{}\n", indent, field.ReferencedServiceName(), field.FieldConfig.References.Resource)
-			}
-			outPrefix += fmt.Sprintf("%s\tif err := getReferencedResourceState_%s(ctx, apiReader, obj, *arr.Name, namespace); err != nil {\n", indent, field.FieldConfig.References.Resource)
-			outPrefix += fmt.Sprintf("%s\t\treturn err\n", indent)
-			outPrefix += fmt.Sprintf("%s\t}\n", indent)
-			outPrefix += fmt.Sprintf("%s\t%s = obj.%s\n", indent, targetVarName, field.FieldConfig.References.Path)
-			outPrefix += fmt.Sprintf("%s}\n", indent)
-		}
-	}
-
-	return outPrefix + outSuffix
 }
 
 func nestedStructNilCheck(path fieldpath.Path, fieldAccessPrefix string) string {
