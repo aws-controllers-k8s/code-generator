b0d76d239d0ab8bd7b642c0914982b5bff389429
Vandita2020 <vandita.vk20@gmail.com>
2023-08-22 12:51:10 -0700

    Changes for ignoring SetS3SHA

--- pkg/config/resource.go
+++ pkg/config/resource.go
@@ -14,6 +14,8 @@
 package config
 
 import (
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	awssdkmodel "github.com/aws/aws-sdk-go/private/model/api"
@@ -557,6 +559,14 @@ func (c *Config) GetResourceFieldName(
 	if !ok {
 		return origFieldName
 	}
+
+	if resourceName == "Function" {
+		if origFieldName == "Code" {
+			t, _ := json.Marshal(rConfig.Renames)
+			fmt.Println("rConfig.Renames", string(t))
+		}
+	}
+
 	if rConfig.Renames == nil {
 		return origFieldName
 	}
